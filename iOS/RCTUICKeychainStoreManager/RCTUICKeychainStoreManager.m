23defb5c31a61bf19ed70f8a43fa533778f52f94
Justin Makaila <justinmakaila@gmail.com>
2015-06-30 11:18:41 -0400

    Removed nullability identifiers because they were causing issues with react native

--- iOS/RCTUICKeychainStoreManager/RCTUICKeychainStoreManager.m
+++ iOS/RCTUICKeychainStoreManager/RCTUICKeychainStoreManager.m
@@ -20,24 +20,24 @@
 
 @implementation RCTUICKeychainStoreManager
 
-RCT_EXPORT_MODULE(RCTUICKeychainManager)
+RCT_EXPORT_MODULE()
 
-RCT_EXPORT_METHOD(setAccessGroup:(nullable NSString *)accessGroup callback:(RCTResponseSenderBlock)callback) {
+RCT_EXPORT_METHOD(setAccessGroup:(NSString *)accessGroup callback:(RCTResponseSenderBlock)callback) {
   self.accessGroup = accessGroup;
   callback(@[[NSNull null], accessGroup]);
 }
 
-RCT_EXPORT_METHOD(setService:(nullable NSString *)service callback:(RCTResponseSenderBlock)callback) {
+RCT_EXPORT_METHOD(setService:(NSString *)service callback:(RCTResponseSenderBlock)callback) {
   self.service = service;
   callback(@[[NSNull null], service]);
 }
 
-RCT_EXPORT_METHOD(setString:(nullable NSString *)string forKey:(NSString *)key callback:(RCTResponseSenderBlock)callback) {
+RCT_EXPORT_METHOD(setString:(NSString *)string forKey:(NSString *)key callback:(RCTResponseSenderBlock)callback) {
   [self setString:string forKey:key service:self.service accessGroup:self.accessGroup callback:callback];
 }
 
 
-RCT_EXPORT_METHOD(setString:(nullable NSString *)string forKey:(NSString *)key service:(nullable NSString *)service accessGroup:(nullable NSString *)accessGroup callback:(RCTResponseSenderBlock)callback) {
+RCT_EXPORT_METHOD(setString:(NSString *)string forKey:(NSString *)key service:(NSString *)service accessGroup:(NSString *)accessGroup callback:(RCTResponseSenderBlock)callback) {
   BOOL success = [UICKeyChainStore setString:string forKey:key service:service accessGroup:accessGroup];
   
   callback(@[[NSNull null], @(success)]);
@@ -48,7 +48,7 @@ RCT_EXPORT_METHOD(stringForKey:(NSString *)key callback:(RCTResponseSenderBlock)
   [self stringForKey:key service:self.service accessGroup:self.accessGroup callback:callback];
 }
 
-RCT_EXPORT_METHOD(stringForKey:(NSString *)key service:(nullable NSString *)service accessGroup:(nullable NSString *)accessGroup callback:(RCTResponseSenderBlock)callback) {
+RCT_EXPORT_METHOD(stringForKey:(NSString *)key service:(NSString *)service accessGroup:(NSString *)accessGroup callback:(RCTResponseSenderBlock)callback) {
   NSError *error = nil;
   NSString *string = [UICKeyChainStore stringForKey:key service:service accessGroup:accessGroup error:&error];
   
