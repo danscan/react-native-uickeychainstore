decd55b765a6570b63ae3c8f402133032d301144
Dan Scanlon <dscanlonpa@gmail.com>
2015-06-30 12:58:09 -0400

    style conformance; simplification

--- index.ios.js
+++ index.ios.js
@@ -1,60 +1,57 @@
 /**
- * @providesModule Keychain
+ * @providesModule react-uickeychainstore
  */
-'use strict';
+let { KeychainManager } = require('NativeModules')
 
-var NativeModules = require('NativeModules')
-var KeychainManager = NativeModules.RCTUICKeychainManager;
-
-var Keychain = {
+let Keychain = {
   // Sets the access group on the default keychain.
-  setAccessGroup: function (accessGroup: string, callback?: ?(error: ?Error) => void): Promise {
+  setAccessGroup(accessGroup: string, callback?: ?(error: ?Error) => void): Promise {
     return new Promise((resolve, reject) => {
-      KeychainManager.setAccessGroup(accessGroup, function (error) {
+      KeychainManager.setAccessGroup(accessGroup, (error) => {
         if (error) {
-          reject(error)
-        } else {
-          resolve()
-        }
+          return reject(error)
+        } 
+
+        return resolve()
       })
     })
   },
 
   // Sets the service on the default keychain.
-  setService: function (service: string, callback?: ?(error: ?Error) => void): Promise {
+  setService(service: string, callback?: ?(error: ?Error) => void): Promise {
     return new Promise((resolve, reject) => {
-      KeychainManager.setService(service, function (error) {
+      KeychainManager.setService(service, (error) => {
         if (error) {
-          reject(error)
-        } else {
-          resolve()
-        }
+          return reject(error)
+        } 
+
+        return resolve()
       })
     })
   },
 
   // Stores the values of `string` in `key`, returning a promise.
-  setString: function (string: string, key: string, callback?: ?(error: ?Error) => void): Promise {
+  setString(string: string, key: string, callback?: ?(error: ?Error) => void): Promise {
     return new Promise((resolve, reject) => {
-      KeychainManager.setString(string, key, function (error) {
+      KeychainManager.setString(string, key, (error) => {
         if (error) {
-          reject(error)
-        } else {
-          resolve()
-        }
+          return reject(error)
+        } 
+
+        return resolve()
       })
     })
   },
 
   // Retrieves the value at `key`, returning a promise.
-  stringForKey: function (key: string, callback?: ?(error: ?Error, result: ?string) => void): Promise {
+  stringForKey(key: string, callback?: ?(error: ?Error, result: ?string) => void): Promise {
     return new Promise((resolve, reject) => {
-      KeychainManager.stringForKey(key, function (error) {
+      KeychainManager.stringForKey(key, (error, result) => {
         if (error) {
-          reject(error)
-        } else {
-          resolve()
-        }
+          return reject(error)
+        } 
+
+        return resolve(result)
       })
     })
   }
